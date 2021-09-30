dce68f7ad6bcdcec429c163fe2473f7d487d9bfb
Yuki326 <88820769+Yuki326@users.noreply.github.com>
2021-09-30 14:01:24 +0900

    演習 formの利用

--- src/App.js
+++ src/App.js
@@ -1,92 +1,49 @@
 import React,{Component} from 'react'
 import './App.css'
-let className="test"
+
 class App extends Component{
-  data =[
-    "This is list Sample",
-    "これはリストのサンプルです",
-    "配列をリストに変換します"
-  ]
-  
-  
+  input = ''
   constructor(props){
     super(props)
-    this.state ={
-      list: this.data,
-      className:"test"
+    this.state={
+      title:'input form',
+      message:'type your name.'
     }
+    this.doChange = this.doChange.bind(this)
+    this.doSubmit = this.doSubmit.bind(this)
   }
-  clicked = () => {
+
+  doChange(event){
+    this.input = event.target.value;
+  }
+
+  doSubmit(event){
     this.setState({
-      className:this.state.className === "test" ? "box":"test"
-    });
-  };
-  
+      title:'send form',
+      message:'Hello,'+this.input+'!!'
+    })
+    event.preventDefault()
+  }
+
   render(){
     return(
       <>
         <h1 className="bg-primary text-white display-4">React</h1>
-        <div className={this.state.className}></div>
-        <button onClick={this.clicked}>test</button>
         <div className="container">
-          <p className="subtitle">Show List.</p>
-          <List title="サンプル・リスト" data={this.data}/>
+          <h4>{this.state.title}</h4>
+          <p className="card h5 p-3">{this.state.message}</p>
+          <div className="alert alert-primary mt-3">
+            <form onSubmit={this.doSubmit}>
+              <div className="form-group">
+                <label>Message:</label>
+                <input type="text" className="form-control" onChange={this.doChange}/>
+              </div>
+              <input type="submit" className="btn btn-primary" value="Click"/>
+            </form>
+          </div>
         </div>
-        <img src={`logo192.png`} alt="Logo" />;
       </>
     )
   }
 }
-
-class List extends Component{
-  number = 1
-  render(){
-    let data=this.props.data;
-    return(
-      <>
-        <p className="h5 text-center">{this.props.title}</p>
-        <ul className="list-group">
-          {
-            data.map
-            (
-              (item,key)=>
-              <li className="list-group-item" key={key}>
-                <Item number={key+1} value={item} />
-              </li>
-            )
-          }
-        </ul>
-      </>
-    )
-  }
-}
-
-class Item extends Component{
-  itm={
-    fontSize:"16pt",
-    color:"#00f",
-    textDecoration:'underline',
-    textDecorationColor:'#ddf'
-  }
-  num={
-    fontWeight:"bold",
-    color:"red"
-  }
-
-  render(){
-    return(
-      <>
-        <p style={this.itm}>
-          <span style={this.num}>
-            [{this.props.number}]&nbsp;
-          </span>
-          {this.props.value}
-          
-          
-        </p>
-      </>
-    )
-  }
-}
-
 export default App;
