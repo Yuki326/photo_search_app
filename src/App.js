bbd08082ca0f654931ad6969825b0f5183d08e45
Yuki326 <88820769+Yuki326@users.noreply.github.com>
2021-09-30 12:40:56 +0900

    演習　cssアニメーション

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React,{Component} from 'react'
 import './App.css'
-
+let className="test"
 class App extends Component{
   data =[
     "This is list Sample",
     "これはリストのサンプルです",
     "配列をリストに変換します"
   ]
+  
+  
   constructor(props){
     super(props)
     this.state ={
-      list: this.data
+      list: this.data,
+      className:"test"
     }
   }
+  clicked = () => {
+    this.setState({
+      className:this.state.className === "test" ? "box":"test"
+    });
+  };
+  
   render(){
     return(
       <>
         <h1 className="bg-primary text-white display-4">React</h1>
+        <div className={this.state.className}></div>
+        <button onClick={this.clicked}>test</button>
         <div className="container">
           <p className="subtitle">Show List.</p>
           <List title="サンプル・リスト" data={this.data}/>
         </div>
+        <img src={`logo192.png`} alt="Logo" />;
       </>
     )
   }
@@ -63,12 +75,16 @@ class Item extends Component{
 
   render(){
     return(
-      <p style={this.itm}>
-        <span style={this.num}>
-          [{this.props.number}]&nbsp;
-        </span>
-        {this.props.value}
-      </p>
+      <>
+        <p style={this.itm}>
+          <span style={this.num}>
+            [{this.props.number}]&nbsp;
+          </span>
+          {this.props.value}
+          
+          
+        </p>
+      </>
     )
   }
 }
